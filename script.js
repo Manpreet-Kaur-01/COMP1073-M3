2c8864762da597c687d15ec800ff9a6bc07f53e3
Manpreet-Kaur-01 <67702401+Manpreet-Kaur-01@users.noreply.github.com>
2021-04-02 15:44:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// reading json
+let URL = "https://manpreet-kaur-01.github.io/COMP1073-M3/weird-deals.json";
+let request = new XMLHttpRequest(); 
+request.open('GET', URL);
+request.responseType='json';
+request.send();
+
+request.onload = function() {
+  let weirdDeals = request.response;
+  // body to add data 
+	let body=document.body;
+  // loop to create elements and add to the data
+  for (let i = 0; i < weirdDeals['deals'].length; i++) {
+	  
+    let div = document.createElement('div');
+    let hTag = document.createElement('h1');
+    let imgTag = document.createElement('img');
+    let h2 = document.createElement('h2');
+    let h3 = document.createElement('h3');
+    let h4 = document.createElement('h4');
+    
+	// adding data to tags
+    hTag.textContent = weirdDeals['deals'][i].name;
+    imgTag.setAttribute('src', weirdDeals['deals'][i].image);
+    imgTag.setAttribute('width', '20%' );    
+    imgTag.setAttribute('height', '20%' );
+    h2.textContent = 'Price is ' + weirdDeals['deals'][i].price;
+    h3.textContent = weirdDeals['deals'][i].description;
+    h4.textContent = 'Main Feature : ' + weirdDeals['deals'][i].feature;
+    // adding data ro body
+    div.appendChild(imgTag); 
+    div.appendChild(hTag);
+    div.appendChild(h2); 
+    div.appendChild(h3);
+    div.appendChild(h4);
+    body.appendChild(div); 
+     
+  }
+  
+}
